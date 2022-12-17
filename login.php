a2ec6b6a4448b7227f4d97c00093e052ebd17345
ChinmayDabholkar <120647903+ChinmayDabholkar@users.noreply.github.com>
2022-12-17 15:01:45 +0000

    asdasd

--- login.php
+++ login.php
@@ -16,74 +16,82 @@ else {
 if(isset($_COOKIE['user'])){
 
 	echo "<div style='color:white; padding:20px; font-size:2.5em; background:tomato;'><b>Block User</b> <br>".$_COOKIE['user']."</div>";
+
 }else{
 
+	$conn = mysqli_connect('localhost','localhost','','grocery');
 
-$conn = mysqli_connect('localhost','localhost','','grocery');
+	$emails = "";
+	$passs = "";
+	if (isset($_POST['login'])) {
 
-$emails = "";
-$passs = "";
-if (isset($_POST['login'])) {
+		$token = filter_var($_POST['token'], FILTER_SANITIZE_STRING);
 
-	$token = filter_var($_POST['token'], FILTER_SANITIZE_STRING);
+			if (isset($_POST['email']) && isset($_POST['password']) && $token && $token == $_SESSION['token'] ) {
+				
+				$user_login = mysqli_real_escape_string($conn,$_POST['email']);
+				$user_login = mb_convert_case($user_login, MB_CASE_LOWER, "UTF-8");	
+				$password_login = mysqli_real_escape_string($conn,$_POST['password']);	
+				
+				$num = 0;
+				$password_login_sha256 = hash('sha256',$password_login);	
+				$tmp_result="SELECT * FROM user WHERE (email='$user_login') AND password='$password_login_sha256'";
+				$result = mysqli_query($conn,$tmp_result);
+				$num = mysqli_num_rows($result);
+				$get_user_email = mysqli_fetch_assoc($result);
+					$get_user_uname_db = $get_user_email['id'];
 
-	if (isset($_POST['email']) && isset($_POST['password']) && $token && $token == $_SESSION['token'] ) {
-		$user_login = mysqli_real_escape_string($conn,$_POST['email']);
-		$user_login = mb_convert_case($user_login, MB_CASE_LOWER, "UTF-8");	
-		$password_login = mysqli_real_escape_string($conn,$_POST['password']);		
-		$num = 0;
-		$password_login_sha256 = hash('sha256',$password_login);
-		$tmp_result="SELECT * FROM user WHERE (email='$user_login') AND password='$password_login_sha256'";
-		$result1 = mysqli_query($conn,$tmp_result);
-		$num = mysqli_num_rows($result1);
-		$get_user_email = mysqli_fetch_assoc($result1);
-			$get_user_uname_db = $get_user_email['id'];
-		if ($num>0) {
-			$_SESSION['user_login'] = $get_user_uname_db;
-			setcookie('user_login', $user_login, time() + (10 * 60), "/");
-			
-			if (isset($_REQUEST['ono'])) {
-				$ono = mysqli_real_escape_string($_REQUEST['ono']);
-				header("location: orderform.php?poid=".$ono."");
-			}else {
-				header('location: index.php');
+						if ($num>0) {
+
+							$_SESSION['user_login'] = $get_user_uname_db;
+							setcookie('user_login', $user_login, time() + (10 * 60), "/");
+							
+							if (isset($_REQUEST['ono'])) {
+								$ono = mysqli_real_escape_string($_REQUEST['ono']);
+								header("location: orderform.php?poid=".$ono."");
+							}else {
+								header('location: index.php');
+							}
+							exit();
+						}else {
+
+							$tmp_result = "SELECT * FROM user WHERE (email='$user_login') AND password='$password_login_sha256'";
+							$result = mysqli_query($conn,$tmp_result);
+							$num = mysqli_num_rows($result);
+							$get_user_email = mysqli_fetch_assoc($result);
+							$get_user_uname_db = $get_user_email['id'];
+
+							if ($num>0) {
+								$emails = $user_login;
+								$activacc ='';
+							}else {
+								$emails = $user_login;
+								$passs = $password_login;
+								$error_message = '<br><br>
+									<div class="maincontent_text" style="text-align: center; font-size: 18px;">
+									<font face="bookman">Email or Password incorrect.<br>
+									</font></div>';
+									$_SESSION['u']+=1;
+									echo "You Enter ".$_SESSION['u']."Time Wrong  UID and Password";
+									echo "<br><a href='login.php'>Try Again</a>";
+									if($_SESSION['u']>2)
+									{
+										header('location:time.php');
+									}
+							}
+						}
+					
 			}
-			exit();
-		}
-		else {
-			$tmp_result = "SELECT * FROM user WHERE (email='$user_login') AND password='$password_login_sha256'";
-			$result1 = mysqli_query($conn,$tmp_result);
-			$num1 = mysqli_num_rows($result1);
-			$get_user_email1 = mysqli_fetch_assoc($result1);
-			$get_user_uname_db1 = $get_user_email1['id'];
-			if ($num1>0) {
-			$emails = $user_login;
-			$activacc ='';
-			}else {
-			$emails = $user_login;
-			$passs = $password_login;
-			$error_message = '<br><br>
-				<div class="maincontent_text" style="text-align: center; font-size: 18px;">
-				<font face="bookman">Email or Password incorrect.<br>
-				</font></div>';
-				$_SESSION['u']+=1;
-				echo "You Enter ".$_SESSION['u']."Time Wrong  UID and Password";
-				echo "<br><a href='login.php'>Try Again</a>";
-				if($_SESSION['u']>2)
-				{
-					 header('location:time.php');
-				}
-		}
-		}
-			
 		}
 	}
-}
 	
 $acemails = "";
 $acccode = "";
+
 if(isset($_POST['activate'])){
+
 	if(isset($_POST['actcode'])){
+
 		$user_login = mysqli_real_escape_string($conn,$_POST['acemail']);
 		$user_login = mb_convert_case($user_login, MB_CASE_LOWER, "UTF-8");	
 		$user_acccode = mysqli_real_escape_string($conn,$_POST['actcode']);
@@ -117,9 +125,9 @@ if(isset($_POST['activate'])){
 				</font></div>';
 	}
 
-
+}
 ?>
-	<meta http-equiv="refresh" content="900">
+	<meta http-equiv="refresh" content="20">
 <!doctype html>
 <html>
 	<head>
@@ -187,12 +195,12 @@ if(isset($_POST['activate'])){
 												echo '
 										<div>
 											<td>
-												<input name="email" placeholder="Enter Your Email" required="required" class="email signupbox" type="email" size="30" value="'.$emails.'">
+												<input name="email" placeholder="Enter Your Email" required="required" class="email signupbox" type="email" size="30">
 											</td>
 										</div>
 										<div>
 											<td>
-												<input name="password" id="password-1" required="required"  placeholder="Enter Password" class="password signupbox " type="password" size="30" value="'.$passs.'">
+												<input name="password" id="password-1" required="required"  placeholder="Enter Password" class="password signupbox " type="password" size="30">
 											</td>
 										</div>
 										<div>
--- time.php
+++ time.php
@@ -1,5 +1,5 @@
 <?php session_start();
 	session_destroy();
-	setcookie('user','Try After 15 min',time()+900);
+	setcookie('user','Try After 20 sec',time()+10);
 	header("location:login.php");
 ?>
